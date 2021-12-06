930ec25be3e6f922df67ed98c478950fb5b97c16
GMShelton <gmflugahorn@gmail.com>
2021-12-06 15:56:03 -0500

    Removed some code

--- ECE2799_Debounce_Test.ino
+++ ECE2799_Debounce_Test.ino
@@ -33,21 +33,21 @@ void setup() {
 
 void loop() {
   int reading = digitalRead(ButtonInput);
-  Serial.println("Press Optional");
+  Serial.println(digitalRead(ButtonInput));
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
   }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
     CurrentPattern ++;
-    Serial.println("Patten Calc");
-    PatternTime = millis();
-     if (PatternTime > PatterenDelay) {
+    Serial.println(CurrentPattern);
+    //PatternTime = millis();
+     //if (PatternTime > PatterenDelay) {
       if (reading != buttonState) {
       buttonState = reading;
       // only toggle the LED if the new button state is HIGH
       if (buttonState == HIGH) {
-        if (CurrentPattern = 1){
+        if (CurrentPattern == 1){
         Serial.println("One push.");
         digitalWrite(LedBlue, LOW);
         digitalWrite(LedRed, HIGH);
@@ -58,7 +58,7 @@ void loop() {
         digitalWrite(LedGreen, HIGH);
         CurrentPattern = 0;
       }
-        if (CurrentPattern = 2){
+        if (CurrentPattern >= 2){
           Serial.println("two push");
         digitalWrite(LedBlue, HIGH);
         digitalWrite(LedRed, HIGH);
@@ -70,24 +70,13 @@ void loop() {
         CurrentPattern = 0;
       }
       
-      if (CurrentPattern = 3){
-        Serial.println("3 push");
-        digitalWrite(LedBlue, HIGH);
-        digitalWrite(LedRed, LOW);
-        digitalWrite(LedGreen, HIGH);
-        delay(1000);
-        digitalWrite(LedBlue, HIGH);
-        digitalWrite(LedRed, HIGH);
-        digitalWrite(LedGreen, HIGH);
-        CurrentPattern = 0;
      }
-     }
-     } 
+      //}
    
   }
 
   // set the LED:
-  digitalWrite(ledPin, ledState);
+  //digitalWrite(ledPin, ledState);
   lastButtonState = reading;
 }
 }
