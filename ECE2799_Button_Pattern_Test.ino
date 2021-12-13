b487d8a7c86545740e959f2405990dd110305cb4
GMShelton <gmflugahorn@gmail.com>
2021-12-13 14:05:19 -0500

    Test 4 PCB

---
+++ ECE2799_Button_Pattern_Test.ino
@@ -0,0 +1,25 @@
+
+#define LedRed 4
+#define LedBlue 0
+#define LedGreen 2
+#define Motor 33
+#define ButtonInput 34
+int ButtonState = 0;
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(Motor,OUTPUT);
+  pinMode(LedRed, OUTPUT);
+  pinMode(LedBlue, OUTPUT);
+  pinMode(LedGreen, OUTPUT);
+  pinMode(ButtonInput,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ButtonState = digitalRead(ButtonInput);
+  if (ButtonState == HIGH) {
+    digitalWrite(Motor,HIGH);
+    digitalWrite(LedBlue,HIGH);
+    delay(2000);
+}
